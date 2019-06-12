6efc5285e37e11ceb8950b0e340134f6a18b34c6
inejc <nejc.ilenic@gmail.com>
2019-06-12 11:36:32 +0200

    transform embeddings to doddle features

--- build.sbt
+++ build.sbt
@@ -1,4 +1,6 @@
 scalaVersion := "2.12.8"
 libraryDependencies ++= Seq(
   "org.tensorflow" % "tensorflow" % "1.13.1",
+  "io.github.picnicml" %% "doddle-model" % "0.0.1-beta3",
+  "org.scalanlp" %% "breeze-natives" % "0.13.2",
 )
--- src/main/scala/semanticweights/Main.scala
+++ src/main/scala/semanticweights/Main.scala
@@ -1,16 +1,22 @@
 package semanticweights
 
+import breeze.linalg.DenseMatrix
+import io.picnicml.doddlemodel.data.Features
 import org.tensorflow.{SavedModelBundle, Tensor}
 
 object Main extends App {
   val model = SavedModelBundle.load("universal_sentence_encoder_large_v3", "serve")
-  val sentences = Array("hello world sentence", "this works!")
-  val embeddings = embed(sentences)
-  model.session().close()
-  println(s"number of embeddings: ${embeddings.length}")
-  println(embeddings)
+  val embeddingsDimension = 512
 
-  def embed(sentences: Array[String]): List[List[Double]] = {
+  val sentences = Array("hello world sentence", "this works!", "we should get three embeddings")
+  val embeddings = embed(sentences)
+
+  model.session().close()
+
+  println(s"shape of the embeddings matrix: (${embeddings.rows}, ${embeddings.cols})")
+  println(s"preview of the embeddings matrix:\n$embeddings")
+
+  def embed(sentences: Array[String]): Features = {
     val computationResult = model
       .session()
       .runner()
@@ -24,10 +30,10 @@ object Main extends App {
   private def prepareInput(sentences: Array[String]): Tensor[String] =
     Tensor.create(sentences.map(_.getBytes("UTF-8")), classOf[String])
 
-  private def getEmbeddings(computationResult: Tensor[_]): List[List[Double]] = {
-    computationResult
-      .copyTo(sentences.map(_ => Array.fill[Float](512) { 0 }))
-      .map(_.map(_.toDouble).toList)
-      .toList
+  private def getEmbeddings(computationResult: Tensor[_]): Features = {
+    val embeddings = computationResult
+      .copyTo(sentences.map(_ => Array.fill[Float](embeddingsDimension) { 0 }))
+      .map(_.map(_.toDouble))
+    DenseMatrix(embeddings:_*)
   }
 }
