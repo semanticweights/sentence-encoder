0921bc4e56ae2da5ff393790a5deb2e8bc967fb0
inejc <nejc.ilenic@gmail.com>
2019-06-12 09:49:41 +0200

    refactor

--- src/main/scala/semanticweights/Main.scala
+++ src/main/scala/semanticweights/Main.scala
@@ -2,29 +2,32 @@ package semanticweights
 
 import org.tensorflow.{SavedModelBundle, Tensor}
 
-
 object Main extends App {
   val model = SavedModelBundle.load("universal_sentence_encoder_large_v3", "serve")
-
   val sentences = Array("hello world sentence", "this works!")
-  val input = prepareInput(sentences)
+  val embeddings = embed(sentences)
+  model.session().close()
+  println(s"number of embeddings: ${embeddings.length}")
+  println(embeddings)
 
-  val computationResult = model
-    .session()
-    .runner()
-    .feed("text_input", input)
-    .fetch("embedded_text")
-    .run()
-    .get(0)
+  def embed(sentences: Array[String]): List[List[Double]] = {
+    val computationResult = model
+      .session()
+      .runner()
+      .feed("text_input", prepareInput(sentences))
+      .fetch("embedded_text")
+      .run()
+      .get(0)
+    getEmbeddings(computationResult)
+  }
 
-  println(computationResult)
-  println(getEmbeddings(computationResult))
-
-  def prepareInput(sentences: Array[String]): Tensor[String] =
+  private def prepareInput(sentences: Array[String]): Tensor[String] =
     Tensor.create(sentences.map(_.getBytes("UTF-8")), classOf[String])
 
-  def getEmbeddings(computationResult: Tensor[_]): List[List[Double]] = {
-    val embeddings = computationResult.copyTo(sentences.map(_ => Array.fill[Float](512) { 0 }))
-    embeddings.map(_.map(_.toDouble).toList).toList
+  private def getEmbeddings(computationResult: Tensor[_]): List[List[Double]] = {
+    computationResult
+      .copyTo(sentences.map(_ => Array.fill[Float](512) { 0 }))
+      .map(_.map(_.toDouble).toList)
+      .toList
   }
 }
